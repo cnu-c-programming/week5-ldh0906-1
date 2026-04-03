5fd14b1ba69bfca3b2e7e7049d1cd4d2dbbab9f0
ldh0906 <a30308104@gmail.com>
2026-04-03 11:54:28 +0900

    2-2 clean

--- code/2-2.c
+++ code/2-2.c
@@ -5,7 +5,7 @@ void sort_strings(char **arr, int size) {
     char *temp;
     for (int i = 0; i < size - 1; i++) {
         for(int j = 0; j < size - i - 1; j++) {
-            if (strcmp(*&arr[j], *&arr[j+1]) > 0) {
+            if (strcmp(arr[j], arr[j+1]) > 0) {
                 char *temp = arr[j];
                 arr[j] = arr[j+1]; 
                 arr[j+1] = temp; 
